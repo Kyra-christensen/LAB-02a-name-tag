db1d4ee4a64225ef952c453974e5b42e6f7566fd
Kyra Christensen <christensen.kyral@gmail.com>
2021-10-06 12:56:45 -0700

    Added input area to change name

---
+++ app.js
@@ -0,0 +1,10 @@
+const updateButton = document.getElementById('update-button');
+const nameInput = document.getElementById('name-input');
+const nameDisplay = document.getElementById('name-display');
+
+
+updateButton.addEventListener('click', () => {
+    const newName = nameInput.value;
+    nameDisplay.textContent = newName;
+    console.log('you clicked the button');
+});
